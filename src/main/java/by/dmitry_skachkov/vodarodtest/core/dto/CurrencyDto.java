6accadf3e38ee7abb8529203ff0f3999d2abe1e4
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 00:35:54 +0300

    Fixed Deserialization problems

--- src/main/java/by/dmitry_skachkov/vodarodtest/core/dto/CurrencyDto.java
+++ src/main/java/by/dmitry_skachkov/vodarodtest/core/dto/CurrencyDto.java
@@ -3,30 +3,62 @@ package by.dmitry_skachkov.vodarodtest.core.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Date;
 
-import java.time.LocalDateTime;
-
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CurrencyDto {
+    @JsonProperty("Cur_ID")
+    private int curId;
 
-    private int Cur_ID;
-    private int Cur_ParentID;
-    private String Cur_Code;
-    private String Cur_Abbreviation;
-    private String Cur_Name;
-    private String Cur_Name_Bel;
-    private String Cur_Name_Eng;
-    private String Cur_QuotName;
-    private String Cur_QuotName_Bel;
-    private String Cur_QuotName_Eng;
-    private String Cur_NameMulti;
-    private String Cur_Name_BelMulti;
-    private String Cur_Name_EngMulti;
-    private int Cur_Scale;
-    private int Cur_Periodicity;
-    private LocalDateTime Cur_DateStart;
-    private LocalDateTime Cur_DateEnd;
+    @JsonProperty("Cur_ParentID")
+    private int curParentId;
+
+    @JsonProperty("Cur_Code")
+    private String curCode;
+
+    @JsonProperty("Cur_Abbreviation")
+    private String curAbbreviation;
+
+    @JsonProperty("Cur_Name")
+    private String curName;
+
+    @JsonProperty("Cur_Name_Bel")
+    private String curNameBel;
+
+    @JsonProperty("Cur_Name_Eng")
+    private String curNameEng;
+
+    @JsonProperty("Cur_QuotName")
+    private String curQuotName;
+
+    @JsonProperty("Cur_QuotName_Bel")
+    private String curQuotNameBel;
+
+    @JsonProperty("Cur_QuotName_Eng")
+    private String curQuotNameEng;
+
+    @JsonProperty("Cur_NameMulti")
+    private String curNameMulti;
+
+    @JsonProperty("Cur_Name_BelMulti")
+    private String curNameBelMulti;
+
+    @JsonProperty("Cur_Name_EngMulti")
+    private String curNameEngMulti;
+
+    @JsonProperty("Cur_Scale")
+    private int curScale;
+
+    @JsonProperty("Cur_Periodicity")
+    private int curPeriodicity;
+
+    @JsonProperty("Cur_DateStart")
+    private Date curDateStart;
+
+    @JsonProperty("Cur_DateEnd")
+    private Date curDateEnd;
 
 }
