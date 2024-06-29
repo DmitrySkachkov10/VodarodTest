428be96353b13dcbc41eaa28f4f7b3e72d26ca09
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-29 22:08:27 +0300

    Initial project structure setup

---
+++ src/main/java/by/dmitry_skachkov/vodarodtest/VodarodTestApplication.java
@@ -0,0 +1,13 @@
+package by.dmitry_skachkov.vodarodtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodarodTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodarodTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/dmitry_skachkov/vodarodtest/VodarodTestApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dmitry_skachkov.vodarodtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodarodTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
