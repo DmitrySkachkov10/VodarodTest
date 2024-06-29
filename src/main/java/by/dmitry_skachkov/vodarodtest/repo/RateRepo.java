1dc0760c118facbb5829ec847f43d80a3881ec5c
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 02:29:50 +0300

    Changed type and name of args

--- src/main/java/by/dmitry_skachkov/vodarodtest/repo/RateRepo.java
+++ src/main/java/by/dmitry_skachkov/vodarodtest/repo/RateRepo.java
@@ -3,12 +3,11 @@ package by.dmitry_skachkov.vodarodtest.repo;
 import by.dmitry_skachkov.vodarodtest.repo.entity.Rate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Repository
 public interface RateRepo extends JpaRepository<Rate, UUID> {
 
-    Rate findByCurrency_CurIdAndDate(int curID, LocalDateTime date);
+    Rate findByCurrency_CurIdAndDate(int code, LocalDate date);
 }
