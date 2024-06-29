7fb7146e12539064a55d56981682ea6115770a72
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 00:40:18 +0300

    Implemented InitService to load initial data to db on application startup

---
+++ src/main/java/by/dmitry_skachkov/vodarodtest/service/InitServiceImp.java
@@ -0,0 +1,42 @@
+package by.dmitry_skachkov.vodarodtest.service;
+
+import by.dmitry_skachkov.vodarodtest.core.dto.CurrencyDto;
+import by.dmitry_skachkov.vodarodtest.repo.CurrencyRepo;
+import by.dmitry_skachkov.vodarodtest.repo.entity.Currency;
+import by.dmitry_skachkov.vodarodtest.service.api.InitService;
+import by.dmitry_skachkov.vodarodtest.service.api.NationalBankClient;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class InitServiceImp implements InitService {
+
+    private final CurrencyRepo currencyRepo;
+
+    private final NationalBankClient client;
+
+    public InitServiceImp(CurrencyRepo currencyRepo, NationalBankClient client) {
+        this.currencyRepo = currencyRepo;
+        this.client = client;
+    }
+
+    @PostConstruct
+    @Override
+    public void loadInitialData() {
+        List<CurrencyDto> currencyDtos = client.getAllAvailableCurrencies();
+
+        List<Currency> currencyEntities = currencyDtos.stream()
+                .map(m -> new Currency(UUID.randomUUID(),
+                        m.getCurId(),
+                        m.getCurAbbreviation(),
+                        m.getCurScale(),
+                        m.getCurName(),
+                        m.getCurCode()))
+                .toList();
+
+        currencyRepo.saveAll(currencyEntities);
+    }
+}
---
+++ src/main/java/by/dmitry_skachkov/vodarodtest/service/api/InitService.java
@@ -0,0 +1,5 @@
+package by.dmitry_skachkov.vodarodtest.service.api;
+
+public interface InitService {
+    void loadInitialData();
+}
