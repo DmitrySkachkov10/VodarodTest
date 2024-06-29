50df9fafdd4bf1aee6ef6cc132391e28aaf6c267
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 00:42:41 +0300

    Fixed deserialization problem

--- src/main/java/by/dmitry_skachkov/vodarodtest/core/dto/RateDto.java
+++ src/main/java/by/dmitry_skachkov/vodarodtest/core/dto/RateDto.java
@@ -1,5 +1,6 @@
 package by.dmitry_skachkov.vodarodtest.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,22 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Data
 public class RateDto {
+
+    @JsonProperty("Cur_ID")
     private int curId;
+
+    @JsonProperty("Date")
     private LocalDate date;
+
+    @JsonProperty("Cur_Abbreviation")
     private String curAbbreviation;
+
+    @JsonProperty("Cur_Scale")
     private int curScale;
+
+    @JsonProperty("Cur_Name")
     private String curName;
+
+    @JsonProperty("Cur_OfficialRate")
     private double curOfficialRate;
 }
