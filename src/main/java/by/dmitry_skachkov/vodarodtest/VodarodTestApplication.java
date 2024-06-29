0a35170124783298491bdb696f7d0b5f30f290df
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 00:36:47 +0300

    Fixed Feign Clients problems

--- src/main/java/by/dmitry_skachkov/vodarodtest/VodarodTestApplication.java
+++ src/main/java/by/dmitry_skachkov/vodarodtest/VodarodTestApplication.java
@@ -2,8 +2,10 @@ package by.dmitry_skachkov.vodarodtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VodarodTestApplication {
 
 	public static void main(String[] args) {
