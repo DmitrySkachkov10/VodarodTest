8e3bff7e44c5e3b64bfbdf0525bd94ec6c46593f
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-29 23:32:08 +0300

    Added feign service

---
+++ src/main/java/by/dmitry_skachkov/vodarodtest/service/api/NationalBankClient.java
@@ -0,0 +1,23 @@
+package by.dmitry_skachkov.vodarodtest.service.api;
+
+import by.dmitry_skachkov.vodarodtest.core.dto.CurrencyDto;
+import by.dmitry_skachkov.vodarodtest.core.dto.RateDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@FeignClient(name = "nationalBankClient", url = "https://api.nbrb.by/exrates")
+public interface NationalBankClient {
+
+    @GetMapping("/currencies")
+    List<CurrencyDto> getAllAvailableCurrencies();
+
+    @GetMapping("/exrates/rates")
+    List<RateDto> getDailyCurrencyRates(@RequestParam("ondate") LocalDate date,
+                                        @RequestParam("periodicity") int periodicity);
+
+}
+
