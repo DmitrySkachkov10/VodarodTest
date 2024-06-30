bc35fa33ab4fb286755fe59e2eaba4f8d82285ad
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 13:58:56 +0300

    update code for aop

--- src/main/java/by/dmitry_skachkov/vodarodtest/service/CurrencyRateServiceImp.java
+++ src/main/java/by/dmitry_skachkov/vodarodtest/service/CurrencyRateServiceImp.java
@@ -2,15 +2,20 @@ package by.dmitry_skachkov.vodarodtest.service;
 
 import by.dmitry_skachkov.vodarodtest.core.dto.ExternalRateDto;
 import by.dmitry_skachkov.vodarodtest.core.dto.RateDto;
+import by.dmitry_skachkov.vodarodtest.core.exception.DataBaseException;
+import by.dmitry_skachkov.vodarodtest.core.exception.FeignClientException;
+import by.dmitry_skachkov.vodarodtest.core.exception.ValidationException;
 import by.dmitry_skachkov.vodarodtest.repo.CurrencyRepo;
 import by.dmitry_skachkov.vodarodtest.repo.RateRepo;
 import by.dmitry_skachkov.vodarodtest.repo.entity.Rate;
 import by.dmitry_skachkov.vodarodtest.service.api.CurrencyRateService;
 import by.dmitry_skachkov.vodarodtest.service.api.NationalBankClient;
+import feign.FeignException;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,26 +36,38 @@ public class CurrencyRateServiceImp implements CurrencyRateService {
         this.currencyRepo = currencyRepo;
     }
 
-    //todo aop around with logs
     @Override
     public void addRatesInfo(LocalDate date) {
-        List<ExternalRateDto> externalRateDtos = client.getDailyCurrencyRates(date, DAILY_FREQUENCY);
+        try {
+            List<ExternalRateDto> externalRateDtos = externalRateDtos = client.getDailyCurrencyRates(date, DAILY_FREQUENCY);
 
-        List<Rate> rateList = externalRateDtos.stream()
-                .map(m -> new Rate(UUID.randomUUID(),
-                        m.getDate().toLocalDate(),
-                        BigDecimal.valueOf(m.getCurOfficialRate()),
-                        currencyRepo.findByCurId(m.getCurId())))
-                .toList();
-        rateRepo.saveAll(rateList);
+            List<Rate> rateList = externalRateDtos.stream()
+                    .map(m -> new Rate(UUID.randomUUID(),
+                            m.getDate().toLocalDate(),
+                            BigDecimal.valueOf(m.getCurOfficialRate()),
+                            currencyRepo.findByCurId(m.getCurId())))
+                    .toList();
+
+            rateRepo.saveAll(rateList);
+        } catch (FeignException e) {
+            throw new FeignClientException(e.getMessage());
+        } catch (Exception e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     @Override
     public RateDto getRateByCodeAndDate(int code, LocalDate date) {
-        Rate rate = rateRepo.findByCurrency_CurIdAndDate(code, date);
-        return new RateDto(rate.getCurrency().getCurAbbreviation(),
-                rate.getCurrency().getCurScale(),
-                rate.getOfficialRate(),
-                rate.getDate());
+        try {
+            Rate rate = rateRepo.findByCurrency_CurIdAndDate(code, date);
+            return new RateDto(rate.getCurrency().getCurAbbreviation(),
+                    rate.getCurrency().getCurScale(),
+                    rate.getOfficialRate(),
+                    rate.getDate());
+        } catch (NullPointerException e) {
+            throw new ValidationException(e.getMessage());
+        } catch (Exception e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 }
