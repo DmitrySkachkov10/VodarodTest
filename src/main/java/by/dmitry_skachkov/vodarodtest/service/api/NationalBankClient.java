3c028d99e65931f89d92a14d37030c761d62becb
DmitrySkachkov10 <dima.dmitry06@gmail.com>
2024-06-30 03:10:34 +0300

    Update url pattern

--- src/main/java/by/dmitry_skachkov/vodarodtest/service/api/NationalBankClient.java
+++ src/main/java/by/dmitry_skachkov/vodarodtest/service/api/NationalBankClient.java
@@ -3,6 +3,7 @@ package by.dmitry_skachkov.vodarodtest.service.api;
 import by.dmitry_skachkov.vodarodtest.core.dto.CurrencyDto;
 import by.dmitry_skachkov.vodarodtest.core.dto.ExternalRateDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,8 +16,9 @@ public interface NationalBankClient {
     @GetMapping("/currencies")
     List<CurrencyDto> getAllAvailableCurrencies();
 
-    @GetMapping("/exrates/rates")
-    List<ExternalRateDto> getDailyCurrencyRates(@RequestParam("ondate") LocalDate date,
+    @GetMapping("/rates")
+    List<ExternalRateDto> getDailyCurrencyRates(@RequestParam("ondate")
+                                                @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
                                                 @RequestParam("periodicity") int periodicity);
 
 }
